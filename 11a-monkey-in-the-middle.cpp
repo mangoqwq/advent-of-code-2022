264ca71183afd89b400f4c6e27f4c86ef61b5009
Max <maxjiang123@gmail.com>
2022-12-11 00:25:58 -0500

    Day 10 🙈

---
+++ 11a-monkey-in-the-middle.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+vector<vector<ll>> items =
+{
+    {63, 84, 80, 83, 84, 53, 88, 72},
+    {67, 56, 92, 88, 84},
+    {52},
+    {59, 53, 60, 92, 69, 72},
+    {61, 52, 55, 61},
+    {79, 53},
+    {59, 86, 67, 95, 92, 77, 91},
+    {58, 83, 89}
+};
+
+vector<vector<ll>> to =
+{
+    {4, 7},
+    {5, 3},
+    {3, 1},
+    {5, 6},
+    {7, 2},
+    {0, 6},
+    {4, 0},
+    {2, 1}
+};
+
+int main(){
+    freopen("txt.in", "r", stdin);
+    freopen("txt.out", "w", stdout);
+    vector<ll> cnt(8);
+    for (int _ = 0; _ < 20; ++_){
+        for (int i = 0; i < 8; ++i){
+            for (ll x : items[i]){
+                cnt[i]++;
+                if (i == 0){
+                    ll y = (x * 11) / 3;
+                    if (y % 13 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 1){
+                    ll y = (x + 4) / 3;
+                    if (y % 11 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 2){
+                    ll y = (x * x) / 3;
+                    if (y % 2 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 3){
+                    ll y = (x + 2) / 3;
+                    if (y % 5 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 4){
+                    ll y = (x + 3) / 3;
+                    if (y % 7 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 5){
+                    ll y = (x + 1) / 3;
+                    if (y % 3 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 6){
+                    ll y = (x + 5) / 3;
+                    if (y % 19 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+                if (i == 7){
+                    ll y = (x * 19) / 3;
+                    if (y % 17 == 0){
+                        items[to[i][0]].push_back(y);
+                    } else{
+                        items[to[i][1]].push_back(y);
+                    }
+                }
+            }
+            items[i].clear();
+        }
+    }
+    sort(cnt.begin(), cnt.end());
+    for (ll x : cnt) cout << x << " ";
+}
