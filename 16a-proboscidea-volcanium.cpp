cb6938a0fa190621776e82950f13fe04c25a283a
Max <maxjiang123@gmail.com>
2022-12-16 19:40:03 -0500

    Day 16 🌋

---
+++ 16a-proboscidea-volcanium.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int memo[20][30][70000];
+
+int main(){
+    freopen("txt.in", "r", stdin);
+    freopen("txt.out", "w", stdout);
+    map<string, int> id;
+    vector<vector<int>> graph(60);
+    vector<int> fid(60), flows;
+    struct Edge{ int to, w; };
+    vector<vector<Edge>> adj(20);
+    set<int> interest;
+    int fT = 0, T = 0;
+    string line;
+    while (getline(cin, line)){
+        istringstream iss(line);
+        string buf;
+        string cur; int f;
+        char a, b, c;
+        iss >> buf >> cur >> buf >> buf >> c >> c >> c >> c >> c;
+        iss >> f >> c >> buf >> buf >> buf >> buf;
+        if (!id.count(cur)) id[cur] = T++;
+        while (true){
+            iss >> a >> b;
+            string to = "";
+            to += a;
+            to += b;
+            if (!id.count(to)) id[to] = T++;
+            graph[id[cur]].push_back(id[to]);
+            if (!(iss >> c)) break;
+        }
+        if (f || cur == "AA"){
+            interest.insert(id[cur]);
+            flows.push_back(f);
+            fid[id[cur]] = fT++;
+        }
+    }
+
+    // compress the graph
+    for (int src : interest){
+        vector<int> dist(60, 1e9);
+        queue<int> q;
+        q.push(src);
+        dist[src] = 0;
+        while (!q.empty()){
+            int v = q.front(); q.pop();
+            for (int to : graph[v]){
+                if (dist[to] == 1e9){
+                    dist[to] = dist[v] + 1;
+                    if (interest.count(to)){
+                        adj[fid[src]].push_back({fid[to], dist[to]});
+                    }
+                    q.push(to);
+                }
+            }
+        }
+    }
+
+    memset(memo, -1, sizeof memo);
+    auto dfs = [&](auto self, int v, int t, int vis) -> int{
+        if (t == -1) return 0;
+        if (memo[v][t][vis] != -1) return memo[v][t][vis];
+        int &ans = memo[v][t][vis];
+        ans = 0;
+        for (auto [to, w] : adj[v]){
+            if (w > t) continue;
+            if (1 & (vis >> to)) continue;
+            int cont = self(self, to, t-w-1, vis + (1 << to));
+            ans = max(ans, cont + (t-w) * flows[to]);
+        }
+        return ans;
+    };
+
+    int src = fid[id["AA"]];
+    int time = 30 - 1;
+    cout << dfs(dfs, src, time, 0) << '\n';
+}
--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Perhaps we can get top hundred??? muri muri ＞︿＜
 | 13  | Distress Signal         | 00:25:04 / 1477th | 00:27:25 / 971th  | 0 + 0 = 0     | 51st        |
 | 14  | Regolith Reservoir      | 00:09:46 / 64th   | 00:19:03 / 279th  | 37 + 0 = 0    | 55th        |
 | 15  | Beacon Exclusion Zone   | 00:13:25 / 214th  | 00:18:14 / 18th   | 0 + 83 = 0    | 51th        |
-| 16  |                         |                   |                   |               |             |
+| 16  | Proboscidea Volcanium   | 00:20:31 / 63rd   | 19:29:34 / 8185th | 38 + 0 = 0    | 50th        |
 | 17  |                         |                   |                   |               |             |
 | 18  |                         |                   |                   |               |             |
 | 19  |                         |                   |                   |               |             |
